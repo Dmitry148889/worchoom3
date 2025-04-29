c2dfbc2b26fce7fec0620dcb03c70b60b15c82a5
Dmitry148889 <saturn148889@gmail.com>
2025-04-29 05:09:44 +0300

    задание 3 выполняется

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Ты можешь водить");
+        }
+
+        if (age < 18) {
+            System.out.println("Ты не можешь водить");
+            System.out.println();
+        }
+
+    }
+}
