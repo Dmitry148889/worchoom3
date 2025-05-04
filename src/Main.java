d1909c516d86cfb49e138916aa409304ac92d8db
Dmitry148889 <saturn148889@gmail.com>
2025-05-04 13:54:43 +0300

    Поправил

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
         if (place <= 60) {
             System.out.println("Если в вагоне место под номером " + place + " то место сидячие");
         } else if (place > 60 && place <= 102) {
-            System.out.println("Если в вагоне место под номером " +place+ ", то в вагоне нет сидячих мест оно будет стоячим");
+            System.out.println("Если в вагоне место под номером " +place+ ", то в вагоне нет сидячих мест оно будет стоячим.");
         } else {
             System.out.println("Вагон заполнен.");
         }
